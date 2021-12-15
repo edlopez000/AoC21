1139fbbd42fbc4bb3e918aaeb81306ba43b692ea
edlopez000 <lopez.edward@outlook.com>
2021-12-15 00:40:33 -0500

    Finished day 1 part 1

---
+++ 1/part1.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: fs.createReadStream("input.txt"),
+  crlfDelay: Infinity,
+});
+
+let increments = 0;
+let lastNum = undefined;
+
+rl.on("line", (currentNum) => {
+  if (lastNum === undefined) {
+    lastNum = Number(currentNum);
+  }
+
+  if (currentNum > lastNum) {
+    increments += 1;
+  }
+  lastNum = Number(currentNum);
+  console.log(increments);
+});
+
+// Not the most optimal solution because increments comes out of the console.log and isn't put in a variable
+// Need to get more familiar with readFileSync
