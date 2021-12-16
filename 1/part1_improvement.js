df59b868795f8ace455b6716cf5d528c4dd05ddd
edlopez000 <lopez.edward@outlook.com>
2021-12-16 01:03:55 -0500

    Changed part2.js to part1_improvement.js which I would say is an improvement over the first iteration

---
+++ 1/part1_improvement.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const readline = require("readline");
+
+const input = fs
+  .readFileSync("input.txt", "utf-8")
+  .split("\n")
+  .map((item) => {
+    return parseInt(item);
+  });
+
+function totalIncrement(arr) {
+  let sum = 0;
+
+  let incrementDetect = (item, index, array) => {
+    if (item > array[index - 1]) {
+      sum += 1;
+    }
+  };
+  arr.forEach(incrementDetect);
+  return sum;
+}
+
+let total = totalIncrement(input);
+
+console.log(`total increment = ${total}`);
